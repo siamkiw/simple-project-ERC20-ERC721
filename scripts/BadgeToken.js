ec1651c05689838fe8914663884e03d4f9145f73
Siam.sae <siam.sae@AGC-C02HD145Q05P.local>
2022-04-05 14:52:15 +0700

    add contract and test

---
+++ scripts/BadgeToken.js
@@ -0,0 +1,17 @@
+const hre = require('hardhat')
+
+async function main(){
+    const BadgeToken = await hre.ethers.getContractFactory("BadgeToken")
+    console.log('Deploying BadgeToken ERC721 token..')
+    const token = await BadgeToken.deploy('BadgeToken2', 'Badge2')
+
+    await token.deployed();
+    console.log("BadgeToken deployed to:", token.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
